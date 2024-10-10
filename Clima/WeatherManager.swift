6ecfea3c6377ae695dab1f870e11428975ded28e
My <uk472363@yanex.ru>
2024-10-10 10:05:00 +0300

    Initial Commit

---
+++ Clima/WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by user on 07.10.2024.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by user on 08.10.2024.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
